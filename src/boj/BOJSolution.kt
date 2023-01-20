67d41f407350f57e26d42df662e72fb30a3acd53
Dev-Joco <csr8367@hanmail.net>
2023-01-21 02:28:05 +0900

    improve: BOJ

--- src/boj/BOJSolution.kt
+++ src/boj/BOJSolution.kt
@@ -33,14 +33,14 @@ abstract class BOJSolution(
 
         printProblemInfo()
 
-        val (userOutputs, times) = runSolution()
+        val userOutputs = runSolutions()
 
-        printTestCases(userOutputs, times)
+        printTestCaseResults(userOutputs)
     }
 
     private fun printProblemInfo() {
         val (problemNo, title, categories, problemUrl, solutionUrl) = info
-        val categoriesString = if (categories.isNotEmpty()) {
+        val categoriesToString = if (categories.isNotEmpty()) {
             categories.joinToString()
         } else {
             "미분류"
@@ -50,73 +50,64 @@ abstract class BOJSolution(
             [문제 정보]
             문제: (BOJ $problemNo) $title
             링크: $problemUrl
-            유형: $categoriesString
+            유형: $categoriesToString
         """.trimIndent())
     }
 
-    private fun runSolution(): Pair<List<String>, List<Double>> {
-        val systemIn = System.`in`
-        val systemOut = System.out
-        val userOutputs: List<String>
-        val times = mutableListOf<Double>()
+    private fun runSolutions(): List<String> = onTestEnvironment { outputStream ->
+        val userOutputs = mutableListOf<String>()
 
-        testCases.joinToString("\n") { it.input }.byteInputStream().use { inputs ->
-            System.setIn(inputs)
+        repeat(testCases.size) {
+            runEachSolution()
 
-            ByteOutputStream().use { output ->
-                PrintStream(output).use { print ->
-                    System.setOut(print)
+            val userOutput = outputStream
+                .newInputStream()
+                .reader()
+                .readLines()
+                .joinToString("\n")
 
-                    repeat(testCases.size) {
-                        val millis = measureTimeMillis {
-                            runEachSolution()
-                        }
-                        times.add(millis / 1000.0)
-                    }
+            userOutputs.add(userOutput)
 
-                    System.setOut(systemOut)
-
-                    userOutputs = output.newInputStream().reader().readLines()
-                }
-            }
-
-            System.setIn(systemIn)
-            System.setOut(systemOut)
+            outputStream.reset()
         }
 
-        return userOutputs to times
+        userOutputs
     }
 
-    private fun printTestCases(userOutputs: List<String>, times: List<Double>) {
+    private fun printTestCaseResults(userOutputs: List<String>) {
         val totalCount = testCases.size
         var correctCount = 0
 
         for ((index, testCase) in testCases.withIndex()) {
             val testCaseNo = index + 1
-            val userOutput = userOutputs[index]
             val (input, answer) = testCase
-            val correct = if (userOutput == answer) "정답(O)" else "오답(X)"
-            val time = times[index]
+            val userOutput = userOutputs[index]
+            val correct = if (userOutput == answer) {
+                "정답(O)"
+            } else {
+                "오답(X)"
+            }
 
             if (userOutput == answer) {
                 correctCount += 1
             }
 
-            println(
-"""-------------------------------
+            val template =
+                """-------------------------------
 [예제 입력 $testCaseNo]
 * input:
 $input
 
 * output:
-${userOutputs[index]}
+$userOutput
 
 * answer:
 $answer
 
-* 결과: $correct, $time 초
+* 결과: $correct
 """.trimIndent()
-            )
+
+            println(template)
         }
 
         println("""
@@ -124,5 +115,30 @@ $answer
             
             * 맞은 개수: 전체 ${totalCount}개 중 ${correctCount}개 맞음
         """.trimIndent())
+    }
+
+    private fun <R> onTestEnvironment(block: (outputStream: ByteOutputStream) -> R): R {
+        val result: R
+        val systemIn = System.`in`
+        val systemOut = System.out
+
+        val inputData = testCases.joinToString("\n") { it.input.trim('\n') }
+
+        inputData.byteInputStream().use { inputStream ->
+            System.setIn(inputStream)
+
+            ByteOutputStream().use { outputStream ->
+                PrintStream(outputStream).use { printStream ->
+                    System.setOut(printStream)
+
+                    result = block(outputStream)
+                }
+            }
+        }
+
+        System.setIn(systemIn)
+        System.setOut(systemOut)
+
+        return result
     }
 }
